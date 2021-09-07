5bec6f369c85ce8e9a090623bd0bc7521e871f59
Charissa Ramirez <chawissa@gmail.com>
2021-09-07 12:42:03 -0400

    Pages and routes

---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nobis
+        mollitia odio hic beatae exercitationem repudiandae deserunt recusandae
+        sapiente. Qui?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nobis
+        mollitia odio hic beatae exercitationem repudiandae deserunt recusandae
+        sapiente. Qui?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nobis
+        mollitia odio hic beatae exercitationem repudiandae deserunt recusandae
+        sapiente. Qui?
+      </p>
+    </div>
+  )
+}
